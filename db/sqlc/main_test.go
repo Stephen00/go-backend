4b8e32ef37928f6782cc73e1b21d8141b1f0fe29
Stephen00 <stephen.graham00@yahoo.com>
2022-12-10 17:39:24 +0000

    fixed unit tests

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -13,17 +13,14 @@ const dbDriver = "postgres"
 const dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 
 var testQueries *Queries
-var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-
-	testDB, err := sql.Open(dbDriver, dbSource)
+	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
 
-	testQueries = New(testDB)
+	testQueries = New(conn)
 
 	os.Exit(m.Run())
 }
